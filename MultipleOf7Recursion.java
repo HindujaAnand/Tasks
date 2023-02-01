75f420eaf64cef1d42c49cbe3c46a6fe1e1bf855
Hinduja <hindujaanand2001@gmail.com>
2023-02-01 11:20:54 +0530

    first commit in git

---
+++ MultipleOf7Recursion.java
@@ -0,0 +1,20 @@
+package Recursion;
+
+public class MultipleOf7Recursion 
+{
+	static void print(int n)
+	{
+		if(n<=100)
+		
+		{
+			if(n%7==0)
+			System.out.println(n);
+			print(n+1);
+		}
+		
+	}
+	public static void main(String[] args) {
+	print(1);
+
+	}
+}
---
+++ OddElementRecursion.java
@@ -0,0 +1,18 @@
+package Recursion;
+
+public class OddElementRecursion 
+{
+	static void print(int n)
+	{
+		if(n<=100)
+		{
+			
+			System.out.println(n);
+			print(n+2);
+		}
+	}
+	public static void main(String[] args) {
+	print(1);
+
+	}
+}
